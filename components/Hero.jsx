2d50ad5890915242b072522ba0c587106606ea37
jthom0734 <82151561+jthom0734@users.noreply.github.com>
2024-04-01 11:06:10 +0100

    Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -40,22 +40,23 @@ const Hero = () => {
           src="assets/img/banner/banner_shape02.png"
           alt=""
           class="alltuchtopdown"
-        /><!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/660a670fa0c6737bd126f1db/1hqca5615';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+        />
       </div>
     </section>
   );
 };
+var Tawk_API = Tawk_API || {};
+var Tawk_LoadStart = new Date();
+
+(function() {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/660a670fa0c6737bd126f1db/1hqca5615';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+
 
 export default Hero;

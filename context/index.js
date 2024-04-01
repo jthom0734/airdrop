6ccc7dff4db161e1ded6dba085806df48568c7bb
jthom0734 <82151561+jthom0734@users.noreply.github.com>
2024-04-01 10:11:17 +0100

    index.js

--- context/index.js
+++ context/index.js
@@ -192,18 +192,6 @@ export const CONTEXT_Provider = ({ children }) => {
       console.log(error);
     }
   };
-var Tawk_API = Tawk_API || {};
-var Tawk_LoadStart = new Date();
-
-(function() {
-    var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/660a670fa0c6737bd126f1db/1hqca5615';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
   
   //ADMIN FUNCTION
   const SET_TOKEN_CONTRACT = async (tokenContract) => {

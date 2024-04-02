3f8d027cc9b52ad8e23b31bd5d8fbb211c3bfefa
jthom0734 <82151561+jthom0734@users.noreply.github.com>
2024-04-02 08:14:46 +0100

    Instagram.jsx

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -1,43 +1,94 @@
-import React, { useState } from "react";
-import { InstagramEmbed 
+import React from 'react';
+import {
+  InstagramEmbed,
   InstagramShareButton,
   InstagramFollowButton,
   InstagramHashtagButton,
   InstagramMentionButton,
-  TwitterTweetEmbed,
   InstagramMomentShare,
   InstagramDMButton,
   InstagramVideoEmbed,
   InstagramOnAirButton,
-} from "react-social-media-embed";
+} from 'react-social-media-embed';
 
-const Instagram = ({ handleClick, user }) => {
+const InstagramComponent = () => {
   return (
-    <>
-      {/*  {user.instagramUrl ? (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <InstagramEmbed url={user.instagramUrl} width={325} height={424} />
-        </div>
-      ) : (
-        <div class="input-group-new">
-          <label class="label-new">Post Url</label>
-          <input onChange={handleClick} class="input-new" type="text" />
-          <div></div>
-        </div>
-      )}*/}
+    <div>
+      {/* Instagram Embed */}
+      <InstagramEmbed
+        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
+        maxWidth={320}
+        hideCaption={false}
+        containerTagName="div"
+        protocol=""
+        injectScript
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
+      />
 
-     <div className="custom-width ">
-        <img src="token.png" alt="" />
-        <h4>Kindly do the following steps:</h4>
-        <TwitterShareButton
-          url={"https://twitter.com/TheBCoders/status/1753257564899545178/"}
-          options={{ text: "Limited airdrop", via: "TheBCoders", height: 400 }}
-        />
-        <InstagramFollowButton screenName={"TheBCoders"} />
-        <InstagramHashtagButton tag={"TheBCoders"} />
-      </div> 
-    </>
+      {/* Instagram Share Button */}
+      <InstagramShareButton
+        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
+        size={32}
+      >
+        Share on Instagram
+      </InstagramShareButton>
+
+      {/* Instagram Follow Button */}
+      <InstagramFollowButton
+        account="openai"
+        size="large"
+        protocol=""
+        color="white"
+        includeMargin
+        appId="12345"
+      >
+        Follow @openai
+      </InstagramFollowButton>
+
+      {/* Instagram Hashtag Button */}
+      <InstagramHashtagButton hashtag="openai">
+        #openai
+      </InstagramHashtagButton>
+
+      {/* Instagram Mention Button */}
+      <InstagramMentionButton username="openai">
+        @openai
+      </InstagramMentionButton>
+
+      {/* Instagram Moment Share */}
+      <InstagramMomentShare />
+      
+      {/* Instagram Direct Message Button */}
+      <InstagramDMButton
+        to="openai"
+        appId="12345"
+      >
+        DM @openai
+      </InstagramDMButton>
+
+      {/* Instagram Video Embed */}
+      <InstagramVideoEmbed
+        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
+        maxWidth={320}
+        hideCaption={false}
+        containerTagName="div"
+        protocol=""
+        injectScript
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
+      />
+
+      {/* Instagram On Air Button */}
+      <InstagramOnAirButton username="openai">
+        On Air
+      </InstagramOnAirButton>
+    </div>
   );
 };
 
-export default Instagram;
+export default InstagramComponent;

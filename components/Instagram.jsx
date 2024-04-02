3f57cb33793c947254f51319ed4663e4e64e3ab0
jthom0734 <82151561+jthom0734@users.noreply.github.com>
2024-04-02 08:24:47 +0100

    Instagram.jsx

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -1,94 +1,38 @@
-import React from 'react';
-import {
-  InstagramEmbed,
-  InstagramShareButton,
-  InstagramFollowButton,
-  InstagramHashtagButton,
-  InstagramMentionButton,
-  InstagramMomentShare,
-  InstagramDMButton,
-  InstagramVideoEmbed,
-  InstagramOnAirButton,
-} from 'react-social-media-embed';
+import React, { useState } from "react";
+import { InstagramEmbed,
+       InstagramShareButton,
+       InstagramFollowButton,
+       InstagramHashtagButton} from "react-social-media-embed";
 
-const InstagramComponent = () => {
+const Instagram = ({ handleClick, user }) => {
   return (
-    <div>
-      {/* Instagram Embed */}
-      <InstagramEmbed
-        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
-        maxWidth={320}
-        hideCaption={false}
-        containerTagName="div"
-        protocol=""
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
-
-      {/* Instagram Share Button */}
-      <InstagramShareButton
-        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
-        size={32}
-      >
-        Share on Instagram
-      </InstagramShareButton>
-
-      {/* Instagram Follow Button */}
-      <InstagramFollowButton
-        account="openai"
-        size="large"
-        protocol=""
-        color="white"
-        includeMargin
-        appId="12345"
-      >
-        Follow @openai
-      </InstagramFollowButton>
-
-      {/* Instagram Hashtag Button */}
-      <InstagramHashtagButton hashtag="openai">
-        #openai
-      </InstagramHashtagButton>
-
-      {/* Instagram Mention Button */}
-      <InstagramMentionButton username="openai">
-        @openai
-      </InstagramMentionButton>
-
-      {/* Instagram Moment Share */}
-      <InstagramMomentShare />
-      
-      {/* Instagram Direct Message Button */}
-      <InstagramDMButton
-        to="openai"
-        appId="12345"
-      >
-        DM @openai
-      </InstagramDMButton>
-
-      {/* Instagram Video Embed */}
-      <InstagramVideoEmbed
-        url="https://www.instagram.com/p/CBd9R_4AJtJ/"
-        maxWidth={320}
-        hideCaption={false}
-        containerTagName="div"
-        protocol=""
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
-
-      {/* Instagram On Air Button */}
-      <InstagramOnAirButton username="openai">
-        On Air
-      </InstagramOnAirButton>
-    </div>
+    <>
+       {/* {user.instagramUrl ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <InstagramEmbed url={user.instagramUrl} width={325} height={424} />
+        </div>
+      ) : (
+        <div class="input-group-new">
+          <label class="label-new">Post Url</label>
+          <input onChange={handleClick} class="input-new" type="text" />
+          <div></div>
+        </div>
+      )}*/}
+     <div className="custom-width ">
+        <img src="token.png" alt="" />
+        <h4>Kindly do the following steps:</h4>
+        <InstagramShareButton
+          url={"https://www.instagram.com/p/CBd9R_4AJtJ/"}
+          options={{ text: "Limited airdrop", via: "TheBCoders", height: 400 }}
+        />
+        <InstagramFollowButton screenName={"TheBCoders"} />
+        <InstagramHashtagButton tag={"TheBCoders"} />
+      </div> 
+    </>
+  );
+};
+    </>
   );
 };
 
-export default InstagramComponent;
+export default Instagram;

e50eb90342c36eec5dbb5ee48756b952b1cbd19a
jthom0734 <82151561+jthom0734@users.noreply.github.com>
2024-04-03 16:40:00 +0100

    Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,11 +14,11 @@ const Hero = () => {
                XECO Pre-Sale and Airdrop for<br />
                 <span> XECO $Iphone is Live</span>
               </h2>
-              <p>
-                <span>Pre-sales CA: 0x52046D05A8C21517553EE8bB65F1763B5c704Cf4</span>
-                <br />
+               <h7 class="title"> <p>
+               Pre-sales CA: 0x4C47f0b663e6b74c9154885F45B9e23637823EDB
+               
                 "Participate in our token sale using the contract address."
-              </p>
+              </p>  </h7>
             </div>
           </div>
         </div>
